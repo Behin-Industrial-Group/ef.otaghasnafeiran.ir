9120279778e72a73e2a76e98365d9155ec994098
mkhodroo <mkhodroo@yahoo.com>
2024-01-17 17:40:56 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,35 +21,35 @@ use Spatie\SimpleExcel\SimpleExcelReader;
 */
 Route::get('/import-user', function () {
     $rows = SimpleExcelReader::create(public_path('Book1.xlsx'))->getRows();
-    echo "<pre>";
-    $rows->each(function(array $rowProperties){
-        if($rowProperties['همراه']){
-            print_r($rowProperties);
-            $province = $rowProperties['استان'];
-            $city = $rowProperties['شهرستان'];
-            $name = "اتاق اصناف شهرستان ". $city;
-            $username = $rowProperties['همراه'];
-            $password = $username;
+    // echo "<pre>";
+    // $rows->each(function(array $rowProperties){
+    //     if($rowProperties['همراه']){
+    //         print_r($rowProperties);
+    //         $province = $rowProperties['استان'];
+    //         $city = $rowProperties['شهرستان'];
+    //         $name = "اتاق اصناف شهرستان ". $city;
+    //         $username = $rowProperties['همراه'];
+    //         $password = $username;
             
-            $city_id = CityController::create($province, $city)->id;
+    //         $city_id = CityController::create($province, $city)->id;
 
-            $user = User::create([
-                'name' => $name,
-                'email' => $username,
-                'password' => Hash::make($password),
-                'city_id' => $city_id
-            ]);
+    //         $user = User::create([
+    //             'name' => $name,
+    //             'email' => $username,
+    //             'password' => Hash::make($password),
+    //             'city_id' => $city_id
+    //         ]);
 
-            UserInfo::create([
-                'user_id' => $user->id,
-                'fname' => $rowProperties['نام'],
-                'lname' => $rowProperties['نام خانوادگی'],
-                'mobile' => $rowProperties['همراه'],
-                'phone' => $rowProperties['تلفن ثابت'],
-                'address' => $rowProperties['آدرس'],
-            ]);
-        }
-    });
+    //         UserInfo::create([
+    //             'user_id' => $user->id,
+    //             'fname' => $rowProperties['نام'],
+    //             'lname' => $rowProperties['نام خانوادگی'],
+    //             'mobile' => $rowProperties['همراه'],
+    //             'phone' => $rowProperties['تلفن ثابت'],
+    //             'address' => $rowProperties['آدرس'],
+    //         ]);
+    //     }
+    // });
 });
 
 Route::get('/build-app', function () {
