e928c76bca2297404621aeeaaf48f6a08436c19d
mkhodroo <mkhodroo@yahoo.com>
2024-01-21 19:39:49 +0330

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,166 +60,295 @@
         }
     </script>
     <style>
-        @import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
-@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+        html {
+            background: url("{{url('public/eid.jpg')}}?{{config('app.version')}}") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+            overflow: hidden;
+			
+        }
 
-body{
-	margin: 0;
-	padding: 0;
-	background: #fff;
+        img {
+            display: block;
+            margin: auto;
+            width: 100%;
+            height: auto;
+			
+        }
 
-	color: #fff;
-	font-family: Arial;
-	font-size: 12px;
-}
+        #login-button {
+            cursor: pointer;
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            padding: 30px;
+            margin: auto;
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            background: rgba(3, 3, 3, .8);
+            overflow: hidden;
+            opacity: 0.4;
+            box-shadow: 10px 10px 30px #000;
+        }
 
-.body{
-	position: absolute;
-	top: -20px;
-	left: -20px;
-	right: -40px;
-	bottom: -40px;
-	width: auto;
-	height: 100vh;
-	background-image: url('{{url("public/eid.jpg")}}');
-	background-size: cover;
-	-webkit-filter: blur(5px);
-	z-index: 0;
-}
+        /* Login container */
+        #container {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+			padding: 10px;
+            width: 340px;
+            height: 500px;
+            border-radius: 5px;
+            background: rgba(255, 255, 255, 0.9);
+            box-shadow: 1px 1px 50px #000;
+            display: none;
+			-webkit-filter: none !important;
+        }
 
-.grad{
-	position: absolute;
-	top: -20px;
-	left: -20px;
-	right: -40px;
-	bottom: -40px;
-	width: auto;
-	height: auto;
-	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
-	z-index: 1;
-	opacity: 0.7;
-}
+        .close-btn {
+            position: absolute;
+            cursor: pointer;
+            font-family: 'Open Sans Condensed', sans-serif;
+            line-height: 18px;
+            top: 3px;
+            right: 3px;
+            width: 20px;
+            height: 20px;
+            text-align: center;
+            border-radius: 10px;
+            opacity: .2;
+            -webkit-transition: all 2s ease-in-out;
+            -moz-transition: all 2s ease-in-out;
+            -o-transition: all 2s ease-in-out;
+            transition: all 0.2s ease-in-out;
+        }
 
-.header{
-	position: absolute;
-	top: calc(50% - 35px);
-	left: calc(50% - 255px);
-	z-index: 2;
-}
+        .close-btn:hover {
+            opacity: .5;
+        }
 
-.header div{
-	float: left;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 35px;
-	font-weight: 200;
-}
+        /* Heading */
+        h1 {
+            font-family: 'Open Sans Condensed', sans-serif;
+            position: relative;
+            margin-top: 0px;
+            text-align: center;
+            font-size: 40px;
+            color: #ddd;
+            text-shadow: 3px 3px 10px #000;
+        }
 
-.header div span{
-	color: #5379fa !important;
-}
+        /* Inputs */
+        a,
+        input {
+            font-family: 'Open Sans Condensed', sans-serif;
+            text-decoration: none;
+            position: relative;
+            width: 80%;
+            display: block;
+            margin: 9px auto;
+            font-size: 17px;
+            color: #fff;
+            padding: 8px;
+            border-radius: 6px;
+            border: none;
+            background: rgba(3, 3, 3, .1);
+            -webkit-transition: all 2s ease-in-out;
+            -moz-transition: all 2s ease-in-out;
+            -o-transition: all 2s ease-in-out;
+            transition: all 0.2s ease-in-out;
+        }
 
-.login{
-	position: absolute;
-	top: calc(50% - 75px);
-	left: calc(50% - 50px);
-	height: 150px;
-	width: 350px;
-	padding: 10px;
-	z-index: 2;
-}
+        input:focus {
+            outline: none;
+            box-shadow: 3px 3px 10px #333;
+            background: rgba(3, 3, 3, .18);
+        }
 
-.login input[type=text]{
-	width: 250px;
-	height: 30px;
-	background: transparent;
-	border: 1px solid rgba(255,255,255,0.6);
-	border-radius: 2px;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 4px;
-}
+        /* Placeholders */
+        ::-webkit-input-placeholder {
+            color: #ddd;
+        }
 
-.login input[type=password]{
-	width: 250px;
-	height: 30px;
-	background: transparent;
-	border: 1px solid rgba(255,255,255,0.6);
-	border-radius: 2px;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 4px;
-	margin-top: 10px;
-}
+        :-moz-placeholder {
+            /* Firefox 18- */
+            color: red;
+        }
 
-.login input[type=button]{
-	width: 260px;
-	height: 35px;
-	background: #fff;
-	border: 1px solid #fff;
-	cursor: pointer;
-	border-radius: 2px;
-	color: #a18d6c;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 6px;
-	margin-top: 10px;
-}
+        ::-moz-placeholder {
+            /* Firefox 19+ */
+            color: red;
+        }
 
-.login input[type=button]:hover{
-	opacity: 0.8;
-}
+        :-ms-input-placeholder {
+            color: #333;
+        }
 
-.login input[type=button]:active{
-	opacity: 0.6;
-}
+        /* Link */
+        a {
+			font-family: Vazir;
+            text-align: center;
+            padding: 4px 8px;
+            background: rgba(107, 255, 3, 1);
+        }
 
-.login input[type=text]:focus{
-	outline: none;
-	border: 1px solid rgba(255,255,255,0.9);
-}
+        a:hover {
+            opacity: 0.7;
+        }
 
-.login input[type=password]:focus{
-	outline: none;
-	border: 1px solid rgba(255,255,255,0.9);
-}
+        #remember-container {
+            position: relative;
+            margin: -5px 20px;
+			color: black;
+        }
 
-.login input[type=button]:focus{
-	outline: none;
-}
+        .checkbox {
+            position: relative;
+            cursor: pointer;
+            -webkit-appearance: none;
+            padding: 5px;
+            border-radius: 4px;
+            background: rgba(3, 3, 3, .2);
+            display: inline-block;
+            width: 16px;
+            height: 15px;
+        }
 
-::-webkit-input-placeholder{
-   color: rgba(255,255,255,0.6);
-}
+        .checkbox:checked:active {
+            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), inset 0px 1px 3px rgba(0, 0, 0, 0.1);
+        }
 
-::-moz-input-placeholder{
-   color: rgba(255,255,255,0.6);
-}
+        .checkbox:checked {
+            background: rgba(3, 3, 3, .4);
+            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05), inset 15px 10px -12px rgba(255, 255, 255, 0.5);
+            color: #fff;
+        }
+
+        .checkbox:checked:after {
+            content: '\2714';
+            font-size: 10px;
+            position: absolute;
+            top: 0px;
+            left: 4px;
+            color: #fff;
+        }
+
+        #remember {
+            position: absolute;
+            font-size: 13px;
+            font-family: 'Hind', sans-serif;
+            color: rgba(255, 255, 255, .5);
+            top: 7px;
+            left: 20px;
+        }
+
+        #forgotten {
+            position: absolute;
+            font-size: 12px;
+            font-family: 'Hind', sans-serif;
+            color: rgba(0, 0, 0, 1);
+            right: 0px;
+            top: 8px;
+			margin-right: 15px;
+            cursor: pointer;
+            -webkit-transition: all 2s ease-in-out;
+            -moz-transition: all 2s ease-in-out;
+            -o-transition: all 2s ease-in-out;
+            transition: all 0.2s ease-in-out;
+        }
+
+        #forgotten:hover {
+            color: rgba(255, 255, 255, .6);
+        }
+
+        #forgotten-container {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            margin: auto;
+            width: 260px;
+            height: 180px;
+            border-radius: 10px;
+            background: rgba(3, 3, 3, 0.25);
+            box-shadow: 1px 1px 50px #000;
+            display: none;
+        }
+
+        .orange-btn {
+            background: rgba(87, 198, 255, .5);
+        }
     </style>
 
 </head>
 
-<div class="body"></div>
-		<div class="grad"></div>
-		<div class="header">
-            
-			<div>
-            <img src="{{ url('public/logo.png') }}" class="col-sm-12" alt="logo" style="width: 150px">
-                
-                اتاق اصناف ایران</div>
+<body>
+    <div id="container">
+		<img src="{{url('public/logo.png')}}" style="width:150px"></img>
+		<p style="text-align: justify">
+			مردم شهر و شهرستان شما امسال با خرید کالای با کیفیت و قیمت های عالی در فضای بسیار زیبا نمایشگاه بهاره،
+			قدردان تلاش‌های صادقانه آن اتاق خواهند بود.
+			امسال با شعار <span style="font-weight: 700">(عید تا عید)</span> به استقبال سالی عالی و باشکوه میرویم
+		</p>
 
-		</div>
-		<br>
-		<div class="login">
-				<input type="text" placeholder="username"  name="user"><br>
-				<input type="password" placeholder="password" name="password"><br>
-				<input type="button" value="Login">
-		</div>
+        <form action="javascript:void(0)" method="post" id="login-form">
+			@csrf
+            <input type="text" name="email" placeholder="{{__('username')}}" autocomplete="off">
+            <input type="password" name="password" placeholder="{{__('password')}}">
+            <a onclick="submit()" >{{__('Log in')}}</a>
+            <div id="remember-container">
+                <span id="forgotten">{{__('Forgotten password')}}</span>
+            </div>
+        </form>
+    </div>
+
+    <!-- Forgotten Password Container -->
+    <div id="forgotten-container">
+        <h1>Forgotten</h1>
+        <span class="close-btn">
+            <img src="{{url('public/logo.png')}}"></img>
+        </span>
+
+        <form>
+            <input type="email" name="email" placeholder="E-mail">
+            <a href="#" class="orange-btn">Get new password</a>
+        </form>
+    </div>
+	<script src="{{ url('public/js/loader.js') . '?' . config('app.version') }}"></script>
+    <script src="{{ url('public/js/clearcach.js') . '?' . config('app.version') }}"></script>
+    <script src="{{ url('public/js/scripts.js') . '?' . config('app.version') }}"></script>
+
+    <script>
+        $("#container").fadeIn();
+    </script>
+	<script>
+        function submit() {
+            send_ajax_request(
+                "{{ route('login') }}",
+                $('#login-form').serialize(),
+                function(response) {
+                    show_message("به صفحه داشبورد منتقل میشوید")
+                    window.location = "{{ url('dashboard') }}"
+                },
+                function(response) {
+                    // console.log(response);
+                    show_error(response)
+                    hide_loading();
+                }
+            )
+        }
+    </script>
+</body>
 
 
 </html>
