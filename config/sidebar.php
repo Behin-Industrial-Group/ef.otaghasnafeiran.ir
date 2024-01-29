56425359a088710c0846bb85cbd8bad0ef278e62
mkhodroo <mkhodroo@yahoo.com>
2024-01-29 11:41:43 +0330

    Update sidebar.php

--- config/sidebar.php
+++ config/sidebar.php
@@ -9,25 +9,10 @@ return [
                 'dashboard' => [ 'fa_name' => 'داشبرد', 'route-name' => '', 'route-url' => 'dashboard' ],
             ]
         ],
-        'cases' => [
-            'fa_name' => 'کارپوشه',
-            'submenu' => [
-                'new-case' => [ 'fa_name' => 'فرایند جدید', 'route-name' => 'MkhodrooProcessMaker.forms.start', 'route-url' => '' ],
-                'inbox' => [ 'fa_name' => 'انجام نشده ها', 'route-name' => 'MkhodrooProcessMaker.forms.todo', 'route-url' => '' ],
-                'done' => [ 'fa_name' => 'انجام شده ها', 'route-name' => 'MkhodrooProcessMaker.forms.done', 'route-url' => '' ],
-                'draft' => [ 'fa_name' => 'پیش نویس', 'route-name' => 'MkhodrooProcessMaker.forms.draft', 'route-url' => '' ]
-            ]
-        ],
         'namayeshgah-info' => [
             'fa_name' => 'اطلاعات نمایشگاه ها',
             'submenu' => [
                 'dashboard' => [ 'fa_name' => 'همه', 'route-name' => 'namayeshgahInfo.form.list', 'route-url' => '' ],
-            ]
-        ],
-        'agencies' => [
-            'fa_name' => 'مراکز',
-            'submenu' => [
-                'dashboard' => [ 'fa_name' => 'همه', 'route-name' => 'agencyInfo.listForm', 'route-url' => '' ],
             ]
         ],
         'users' => [
@@ -35,35 +20,6 @@ return [
             'submenu' => [
                 'dashboard' => [ 'fa_name' => 'همه', 'route-name' => 'user.listForm', 'route-url' => 'admin/user/all' ],
                 'role' => [ 'fa_name' => 'نقش ها', 'route-name' => 'role.listForm', 'route-url' => '' ],
-            ]
-        ],
-        'reports' => [
-            'fa_name' => 'گزارش',
-            'submenu' => [
-                'issues' => [ 'fa_name' => 'تیکت', 'route-name' => '', 'route-url' => 'admin/report/ticket' ],
-                'call' => [ 'fa_name' => 'تماس', 'route-name' => '', 'route-url' => 'admin/report/call' ],
-                'guild' => [ 'fa_name' => 'پروانه کسب', 'route-name' => '', 'route-url' => 'admin/report/license' ],
-                'irngv' => [ 'fa_name' => 'نظرسنجی irngv', 'route-name' => 'report.irngv.poll', 'route-url' => '' ],
-            ]
-        ],
-        'asign-ins' => [
-            'fa_name' => 'تخصیص بازرس',
-            'submenu' => [
-                'new' => [ 'fa_name' => 'درخواست جدید', 'route-name' => '', 'route-url' => 'admin/ins/asign/ins/form' ],
-                'list' => [ 'fa_name' => 'لیست درخواست ها', 'route-name' => '', 'route-url' => 'admin/ins/show/all' ],
-            ]
-        ],
-        'ins-requests' => [
-            'fa_name' => 'لیست درخواست ها',
-            'submenu' => [
-                'list' => [ 'fa_name' => 'همه', 'route-name' => '', 'route-url' => 'admin/request/asign/ins/show/all' ],
-            ]
-        ],
-        'irngv-poll' => [
-            'fa_name' => 'اطلاعات دریافتی از irngv',
-            'submenu' => [
-                'irngv' => [ 'fa_name' => 'اطلاعات دریافتی', 'route-name' => 'admin.irngv.show.list', 'route-url' => '' ],
-                'poll' => [ 'fa_name' => 'اطلاعات نظرسنجی', 'route-name' => 'admin.irngv.show.answers', 'route-url' => '' ],
             ]
         ],
         'tickets' => [
